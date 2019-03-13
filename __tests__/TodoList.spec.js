2b3c808a9409fb15381925870d1481b25d231f5c
Josh Justice <me@codingitwrong.com>
2019-03-12 21:04:03 -0400

    Add placeholder test

---
+++ __tests__/TodoList.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, fireEvent } from 'react-native-testing-library';
+import TodoList from '../TodoList';
+
+describe('TodoList', () => {
+  it('displays passed-in todos', () => {
+    fail('Implement me!');
+  });
+
+  describe('adding a todo', () => {
+    it.skip('displays the new todo', () => {
+      fail('Implement me!');
+    });
+
+    it.skip('clears the text field', () => {
+      fail('Implement me!');
+    });
+  });
+
+  it.skip('allows deleting a todo', () => {
+    fail('Implement me!');
+  });
+});
