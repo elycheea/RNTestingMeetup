7cbe9ea381a7f36ea8f700937b7628d95012f970
Josh Justice <me@codingitwrong.com>
2019-04-11 07:30:11 -0400

    Implement component test

--- __tests__/TodoList.spec.js
+++ __tests__/TodoList.spec.js
@@ -4,20 +4,53 @@ import TodoList from '../TodoList';
 
 describe('TodoList', () => {
   it('displays passed-in todos', () => {
-    fail('Implement me!');
+    const todoName = 'My Todo';
+    const { queryByText } = render(<TodoList todos={[todoName]} />);
+
+    expect(queryByText(todoName)).not.toBeNull();
   });
 
   describe('adding a todo', () => {
-    it.skip('displays the new todo', () => {
-      fail('Implement me!');
+    const todoName = 'My Todo';
+
+    let getByTestId;
+    let queryByText;
+
+    describe('when a name is entered', () => {
+      beforeEach(() => {
+        ({ getByTestId, queryByText } = render(<TodoList />));
+
+        fireEvent.changeText(getByTestId('todoName'), todoName);
+        fireEvent.press(getByTestId('addTodoButton'));
+      });
+
+      it('displays the new todo', () => {
+        expect(queryByText(todoName)).not.toBeNull();
+      });
+
+      it('clears the text field', () => {
+        expect(getByTestId('todoName').props.value).toEqual('');
+      });
     });
 
-    it.skip('clears the text field', () => {
-      fail('Implement me!');
+    describe('when no name is entered', () => {
+      it('does not display a row for the new todo', () => {
+        const { getByTestId, queryByTestId } = render(<TodoList />);
+
+        fireEvent.changeText(getByTestId('todoName'), '');
+        fireEvent.press(getByTestId('addTodoButton'));
+
+        expect(queryByTestId('deleteTodoButton')).toBeNull();
+      });
     });
   });
 
-  it.skip('allows deleting a todo', () => {
-    fail('Implement me!');
+  it('allows deleting a todo', () => {
+    const todoName = 'My Todo';
+    const { getByTestId, queryByText } = render(<TodoList todos={[todoName]} />);
+
+    fireEvent.press(getByTestId('deleteTodoButton'));
+
+    expect(queryByText(todoName)).toBeNull();
   });
 });
