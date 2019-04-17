cc582bd1f99ca55ea3e68bf1442ac8a08539d613
elysia <elysia.hwang@gmail.com>
2019-04-16 23:08:31 -0400

    Add react-native-testing-library tests

--- __tests__/TodoList.spec.js
+++ __tests__/TodoList.spec.js
@@ -4,20 +4,35 @@ import TodoList from '../TodoList';
 
 describe('TodoList', () => {
   it('displays passed-in todos', () => {
-    fail('Implement me!');
+    const mockTodos = ['Buy milk', 'Feed dog'];
+    const { queryByText } = render(<TodoList todos={ mockTodos }/>);
+
+    mockTodos.forEach((todo) => {
+      expect(queryByText(todo)).not.toBeNull();
+    });
   });
 
   describe('adding a todo', () => {
-    it.skip('displays the new todo', () => {
-      fail('Implement me!');
+    let getByTestId;
+
+    beforeEach(() => {
+      ({ getByTestId } = render(<TodoList/>));
+      fireEvent.changeText(getByTestId('todoName'), 'Do laundry');
+      fireEvent.press(getByTestId('addTodoButton'));
     });
 
-    it.skip('clears the text field', () => {
-      fail('Implement me!');
+    it('displays the new todo', () => {
+    });
+
+    it('clears the text field', () => {
+      expect(getByTestId('todoName').props.value).toEqual('');
     });
   });
 
-  it.skip('allows deleting a todo', () => {
-    fail('Implement me!');
+  it('allows deleting a todo', () => {
+    const { getByTestId, queryByText } = render(<TodoList todos={['Do laundry']}/>);
+
+    fireEvent.press(getByTestId('deleteTodoButton'));
+    expect(queryByText('Do laundry')).toBeNull();
   });
 });
